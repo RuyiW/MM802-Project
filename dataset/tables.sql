b6e1607b286e27d9cfc4314507ebae77810e0cd4
ruyi1 <ruyi1@ualberta.ca>
2016-04-05 23:00:53 -0600

    join is finished

--- dataset/tables.sql
+++ dataset/tables.sql
@@ -155,6 +155,30 @@ CREATE TABLE IF NOT EXISTS 311_request_status (
 	transit_ref_number INT(10)
 );
 
+CREATE TABLE IF NOT EXISTS temp_311(
+	ticket_number bigint(10), 
+	date_created TIMESTAMP,
+	date_closed TIMESTAMP,
+	311_request_status VARCHAR(5) NOT NULL,
+	311_status_detail VARCHAR(20),
+	service_category VARCHAR(20),
+	service_details VARCHAR(30),
+	business_unit VARCHAR(50),
+	311_neighbourhood VARCHAR(20),
+	community_league VARCHAR(60),
+	311_ward VARCHAR(7),
+	address VARCHAR(60),
+	311_latitude FLOAT(20, 15),
+	311_longtitude FLOAT(20, 15),
+	311_location_x FLOAT(20, 15),
+	311_location_y FLOAT(20, 15),
+	ticket_source VARCHAR(15),
+	calendar_year YEAR(4),
+	311_count INT(1),
+	posse_number VARCHAR(13),
+	transit_ref_number INT(10)
+);
+
 CREATE TABLE IF NOT EXISTS checked_311_result (
 	ticket_number bigint(10), 
 	date_created TIMESTAMP,
@@ -206,7 +230,12 @@ CREATE TABLE IF NOT EXISTS match_resultNeighbourhood (
 	matched_ticket_number bigint(10),
 	complaint_number INT(3)
 );
+
 CREATE TABLE IF NOT EXISTS match_resultdistance (
+	complaint_number INT(3),
+	matched_ticket_number bigint(10),
+	distkm FLOAT(5, 3)
+);CREATE TABLE IF NOT EXISTS match_resultdistance (
 	complaint_number INT(3),
 	matched_ticket_number bigint(10),
 	distkm FLOAT(5, 3)
