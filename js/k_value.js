92eeaaf5eb24000fb8c7d7cd926cd1a57720f527
ruyi1 <ruyi1@ualberta.ca>
2016-04-10 16:27:05 -0600

    update slide bar
    can show selected value immediately

--- js/k_value.js
+++ js/k_value.js
@@ -3,12 +3,14 @@ function pass_k_value(ele){
 	if(ele.id == "value_for_days"){
 		value = ele.value;
 		$('#empty_div').load('ProjNNdays.php?k_value=' +value);
+		//document.getElementById("day_value").innerHTML = value;
 		console.log("value of days");
 		console.log(value);
 	}
 	else if(ele.id == "value_for_distance"){
 		value = ele.value;
 		$('#empty_div').load('ProjNNdistance.php?k_value=' +value);
+		//document.getElementById("distance_value").innerHTML = value;
 		console.log("value of distance");
 		console.log(value);
 	}
--- knn_new.php
+++ knn_new.php
@@ -35,17 +35,21 @@
 				<ul>
 					<li><a href="ProjNNneighbour.php">By Neighbourhood</a></li>
 					<li><a href="ProjNNdays.php">By Days</a>
-						<form method = "post">
+						<form oninput="current_day_value.value=parseInt(value_for_days.value)" method = "post">
 							<p>Please specify the range of days: (0-7)</p>
 							<input id = "value_for_days" type="range" name="points" min="0" max="7" value="0" onchange = "pass_k_value(this)">
+							<output name = "current_day_value" for="value_for_days" style="color:white"></output>
 						</form>
+						<!-- <p style = "color:white;" id = "day_value"></p> -->
 					</li>
 					<li>
 						<a href="ProjNNdistance.php">By Distance</a>
-						<form method = "post">
+						<form oninput="current_distance_value.value=parseInt(value_for_distance.value)" method = "post">
 							<p>Please specify the range of days: (0-7)</p>
 							<input id = "value_for_distance" type="range" name="points" min="0" max="7" value = "0" onchange = "pass_k_value(this)">
+							<output name = "current_distance_value" for="value_for_distance" style="color:white"></output>
 						</form>
+						<!-- <p style = "color:white;" id = "distance_value"></p> -->
 					</li>
 				</ul>
 			</div>
