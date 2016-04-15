1d012ad52ceefbcf02fde217c60397a21f1d231c
swetabedmutha09 <bedmutha@ualberta.ca>
2016-04-14 18:30:00 -0600

    bar chart to close req type 2

---
+++ chart2.php
@@ -0,0 +1,116 @@
+<?php session_start(); 
+  $host="localhost";
+  $db_user="root";
+  $db_pass="";
+  $db_name="db_relations_short";
+  // Create connection
+  $conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+?>
+
+<?php
+
+$index = 0;
+$get_data_sql1 = "SELECT DISTINCT 311_neighbourhood,311_request_status,service_category FROM 311_Explorer";
+$result1 = $conn->query($get_data_sql1);
+$rows1 = array();
+if ($result1->num_rows > 0) {
+  $index = 0;
+  while($rr = $result1->fetch_assoc()) {
+      $CompareReq_Status = strcasecmp($rr["311_request_status"] , 'Close');
+      $Compare_ServiceCategory = strcasecmp($rr["service_category"] , '2');
+      if(($CompareReq_Status == 0) && ($Compare_ServiceCategory == 0)){
+         $rows1[$index] = $rr["311_neighbourhood"];
+         $index++;
+      }
+    }
+}
+
+$get_data_sql = "SELECT 311_neighbourhood,311_request_status,service_category,311_count,date_created,date_closed FROM 311_Explorer";
+$result = $conn->query($get_data_sql);
+$rows = array();
+if ($result->num_rows > 0) {
+  $i = 0;
+  while($r = $result->fetch_assoc()) {
+    $CompareReq_Status = strcasecmp($r["311_request_status"] , 'Close');
+     $Compare_ServiceCategory = strcasecmp($r["service_category"] , '2');
+      if(($CompareReq_Status == 0) && ($Compare_ServiceCategory ==0)){
+         $rowsneighbour[$i] = ($r["311_neighbourhood"]);
+         $rowsbylawcount[$i] = ($r["311_count"]);
+         $rowsdatecreated[$i] = ($r["date_created"]);
+         $rowsdateclosed[$i] = ($r["date_closed"]);
+         $i++;
+      }
+    }
+}
+//print $i;
+for($ii = 0; $ii < $index; $ii++){
+    $neighbour = $rows1[$ii];
+    $count = 0;
+    $tempcount = 0;
+   for($jj = 0; $jj < $i; $jj++){
+    $neighbour1 = $rowsneighbour[$jj];
+    $compare_neighbourhood = strcasecmp($neighbour, $neighbour1);
+    if($compare_neighbourhood == 0){
+            $dateValue = $rowsdatecreated[$jj];
+      $dateValue1 = $rowsdateclosed[$jj];
+      //Date created
+      $time  = strtotime($dateValue);
+       $d  = date('d',$time);
+     //  echo $d . "<br />\n";
+        $m  = date('m',$time);
+      //   echo $m . "<br />\n";
+        $y  = date('Y',$time);
+        //Date closed
+      $time1  = strtotime($dateValue1);
+        $d1  = date('d',$time1);
+        $m1  = date('m',$time1);
+    //     echo $m1 . "<br />\n";
+        $y1  = date('Y',$time1);
+        if ($m == $m1 ){
+             $count += ($d1-$d);
+        }
+        else
+        {
+          $distance = 0;
+          if($m1 > $m){
+            for($mon = $m; $mon <$m1 ; $mon++){
+               if($mon == 1 || $mon == 3 || $mon== 5 || $mon== 7 || $mon== 8 || $mon== 10 || $mon == 12){
+                  $distance += 31 - $d;
+                } 
+              else{
+                  if($mon == 2){
+                    if((($y % 4) == 0 && (($y % 100) != 0)) || ($y % 400) == 0)
+                    {
+                      $distance +=  29 - $d; 
+                    }
+                    else{
+                      $distance +=  28 - $d; 
+                    }
+                    
+                  }
+                  else{
+                     $distance +=  30 - $d; 
+                  }
+                 
+                }
+            }
+            $count += ($distance + $d1);
+          }
+        }
+        $tempcount++;
+        $rows[$ii] = (int)($count/$tempcount);
+    }
+
+   }
+}
+$result2 = array();
+array_push($result2,$rows1);
+array_push($result2,$rows);
+
+print json_encode($result2);
+
+?>
---
+++ display2.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Highcharts Example</title>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript">
+        var charts;
+        $(document).ready(function() {
+            var options = {
+                chart: {
+                    renderTo: 'container',
+                    type: 'column',
+                    //marginRight: 130,
+                    //marginBottom: 25
+                },
+                title: {
+                    text: 'Neighbourhood Graffiti and Vandalism Requests',
+                    x: -20,//center
+                      style: {
+                        color: '#FF0011',
+                        fontWeight: 'bold'
+                    }
+                },
+                subtitle: {
+                    text: 'Average time in days to close the request',
+                    x: -20,
+                     style: {
+                        color: '#FF00FF',
+                        fontWeight: 'bold'
+                    }
+                },
+                xAxis: {
+                     title: {
+                        text: 'Neighbourhood'
+                    },
+                    categories: []
+                },
+                 yAxis: {
+                    
+                     title: {
+                        text: 'Average days'
+                    },
+                     plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                  plotOptions: {
+                    series: {
+                        colorByPoint: true
+                    }
+                },
+                // yAxis: {
+                //     title: {
+                //         text: 'Amount'
+                //     },
+                //     plotLines: [{
+                //         value: 0,
+                //         width: 1,
+                //         color: '#808080'
+                //     }]
+                // },
+                tooltip: {
+                    formatter: function() {
+                            return '<b>'+ this.series.name +'</b><br/>'+
+                            this.x +': '+ this.y;
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'top',
+                    x: -10,
+                    y: 100,
+                    borderWidth: 0
+                },
+               // series: []
+                   series:[{
+                   name: "Average days",
+                   data: []
+                  }]
+            }
+            
+            $.getJSON("chart2.php", function(json) {
+                // options.xAxis.categories = json[0];//['Bylaw Neighbourhood'];
+                // options.yAxis.categories = json[1];//['Bylaw Neighbourhood'];
+                // options.series[0] = json[0];
+                // options.series[1] = json[1];
+                    yData = options.series[0].data; //Array to store data for y column
+                    xData = options.xAxis.categories; //Array to store data for x column
+
+                    xDataObj = json[0];
+                    yDataObj = json[1];
+
+                    for(var key in xDataObj){
+                         xData.push(xDataObj[key]);
+                    }
+                    
+                    for(var key in yDataObj){
+                        yData.push(parseFloat(yDataObj[key]));
+                    }
+                chart = new Highcharts.Chart(options);
+            });
+        });
+        </script>
+        <script src="http://code.highcharts.com/highcharts.js"></script>
+        <script src="http://code.highcharts.com/modules/exporting.js"></script>
+    </head>
+    <body>
+        <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
+    </body>
+</html>
